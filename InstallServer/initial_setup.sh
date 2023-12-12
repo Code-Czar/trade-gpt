7dc15d1fd793af48ada4c71f759756b38d1959ca
romainhk <romainhk@gmail.com>
2023-12-12 08:18:53 +0100

    fix: some fixes about install scripts

--- InstallServer/initial_setup.sh
+++ InstallServer/initial_setup.sh
@@ -2,6 +2,8 @@
 
 # Define the log file path
 set log_file "initial_setup.log"
+# # Enable logging to both file and stdout
+log_file -a $log_file
 
 # Source the common library file
 source utils.exp
@@ -29,7 +31,8 @@ execute_remote $remote_user $remote_ip $remote_port $root_password "useradd -m o
 execute_remote $remote_user $remote_ip $remote_port $root_password "mkdir -p /var/www/trading-shared && chown -R opDevUser:www-data /var/www && chmod -R g+rw  www-data:www-data && usermod -a -G www-data opDevUser" $log_file
 
 # Generate SSH key and set up authorized_keys
-execute_remote $remote_user $remote_ip $remote_port $root_password "runuser -l opDevUser -c 'ssh-keygen -t rsa -N \"\" -f ~/.ssh/id_rsa <<<n'" $log_file
+execute_remote $remote_user $remote_ip $remote_port $root_password "mkdir /home/opDevUser/.ssh && chown opDevUser:opDevUser /home/opDevUser/.ssh" $log_file
+execute_remote $remote_user $remote_ip $remote_port $root_password "runuser -l opDevUser -c 'ssh-keygen -t rsa -N \"\" -f ~/.ssh/id_rsa'" $log_file
 
 # Add public keys to authorized_keys
 foreach key_file $public_keys_files {
@@ -38,7 +41,7 @@ foreach key_file $public_keys_files {
 }
 
 # Change SSH incoming port
-execute_remote $remote_user $remote_ip $remote_port $root_password "sed -i 's/#Port 22/Port 2233/' /etc/ssh/sshd_config && systemctl restart sshd" $log_file
+#execute_remote $remote_user $remote_ip $remote_port $root_password "sed -i 's/#Port 22/Port 2233/' /etc/ssh/sshd_config && systemctl restart sshd" $log_file
 
 # Install rsync on the remote server
 execute_remote $remote_user $remote_ip $remote_port $root_password "apt-get update && apt-get install -y rsync xsel xclip tmux build-essential python3-pip python3-venv influxdb postgresql postgresql-contrib ufw" $log_file
